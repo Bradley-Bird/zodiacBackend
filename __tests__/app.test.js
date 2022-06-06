2aeea4dac6b0bcb36d31514a22493b8f990ba272
Bradley-Bird <Bradleybird2@icloud.com>
2022-06-06 15:21:41 -0700

    first test passing, getting  zodiac names and ids

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,9 +3,22 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+const { zodiac } = require('../data/zodiac');
+
 describe('zodiacBackend routes', () => {
   beforeEach(() => {
     return setup(pool);
+  });
+
+  it('/zodiac returns a list of zodiac signs ids and names', async () => {
+    const resp = await request(app).get('/zodiac');
+    const expected = zodiac.map(({ id, name }) => {
+      return {
+        id,
+        name,
+      };
+    });
+    expect(resp.body).toEqual(expected);
   });
 
   afterAll(() => {
